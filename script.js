df08edf102ec42bf2b8ffc201f8c6727bcd408d3
Patrick Morrison <patgramo@gmail.com>
2022-06-23 14:35:18 -0700

    Added CSS for desktop

---
+++ script.js
@@ -0,0 +1,16 @@
+function showDropdown() {
+    document.getElementById("dropdown-show").classList.toggle("show");
+};
+
+window.onclick = function(event) {
+    if (!event.target.matches('.hamburger')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
